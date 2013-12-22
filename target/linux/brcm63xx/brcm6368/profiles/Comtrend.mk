09309df053647aeca6581e3319479ea31e605c62
Álvaro Fernández Rojas <noltari@gmail.com>
2013-12-22 23:16:15 +0100

    [brcm63xx] Add support for Comtrend VR-3025un.

--- target/linux/brcm63xx/brcm6368/profiles/Comtrend.mk
+++ target/linux/brcm63xx/brcm6368/profiles/Comtrend.mk
@@ -6,3 +6,12 @@ define Profile/VR3025U/Description
 	Package set for the VR-3025u
 endef
 $(eval $(call Profile,VR3025U))
+
+define Profile/VR3025UN
+  NAME:=Comtrend VR-3025un
+  PACKAGES:=kmod-brcm-wl kmod-usb-core kmod-usb-ohci kmod-usb2 wlc
+endef
+define Profile/VR3025UN/Description
+	Package set for the VR-3025un
+endef
+$(eval $(call Profile,VR3025UN))
