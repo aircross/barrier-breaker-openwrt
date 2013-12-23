ff97e349263483861211998ed936448220aec25a
Álvaro Fernández Rojas <noltari@gmail.com>
2013-12-23 13:02:09 +0100

    [brcm63xx] Switch to b43 on Comtrend VR-3025u and VR-3025un.

--- target/linux/brcm63xx/brcm6368/profiles/Comtrend.mk
+++ target/linux/brcm63xx/brcm6368/profiles/Comtrend.mk
@@ -1,6 +1,6 @@
 define Profile/VR3025U
   NAME:=Comtrend VR-3025u
-  PACKAGES:=kmod-brcm-wl kmod-usb-core kmod-usb-ohci kmod-usb2 wlc
+  PACKAGES:=kmod-b43 kmod-usb-core kmod-usb-ohci kmod-usb2 wpad-mini
 endef
 define Profile/VR3025U/Description
 	Package set for the VR-3025u
@@ -9,7 +9,7 @@ $(eval $(call Profile,VR3025U))
 
 define Profile/VR3025UN
   NAME:=Comtrend VR-3025un
-  PACKAGES:=kmod-brcm-wl kmod-usb-core kmod-usb-ohci kmod-usb2 wlc
+  PACKAGES:=kmod-b43 kmod-usb-core kmod-usb-ohci kmod-usb2 wpad-mini
 endef
 define Profile/VR3025UN/Description
 	Package set for the VR-3025un
