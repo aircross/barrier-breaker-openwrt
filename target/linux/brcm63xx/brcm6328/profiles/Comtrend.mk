2505d4f5927ce4c0f180684492731901b36ef5a5
Álvaro Fernández Rojas <noltari@gmail.com>
2014-02-08 16:45:52 +0100

    [brcm63xx] Add kmod-usb-ohci to 6328 devices.

--- target/linux/brcm63xx/brcm6328/profiles/Comtrend.mk
+++ target/linux/brcm63xx/brcm6328/profiles/Comtrend.mk
@@ -1,6 +1,6 @@
 define Profile/AR5381U
   NAME:=Comtrend AR-5381u
-  PACKAGES:=kmod-brcmsmac kmod-usb-core kmod-usb2 wpad-mini
+  PACKAGES:=kmod-brcmsmac kmod-usb-core kmod-usb-ohci kmod-usb2 wpad-mini
 endef
 define Profile/AR5381U/Description
 	Package set for the AR-5381u
@@ -9,7 +9,7 @@ $(eval $(call Profile,AR5381U))
 
 define Profile/AR5387UN
   NAME:=Comtrend AR-5387un
-  PACKAGES:=kmod-brcmsmac kmod-usb-core kmod-usb2 wpad-mini
+  PACKAGES:=kmod-brcmsmac kmod-usb-core kmod-usb-ohci kmod-usb2 wpad-mini
 endef
 define Profile/AR5387UN/Description
 	Package set for the AR-5387un
