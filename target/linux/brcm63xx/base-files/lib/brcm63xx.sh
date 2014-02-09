6efaf047835b2150fcf64177e4432fc74bb409ec
Álvaro Fernández Rojas <noltari@gmail.com>
2014-02-09 14:49:52 +0100

    [brcm63xx] Add support for Comtrend WAP-5813n.

--- target/linux/brcm63xx/base-files/lib/brcm63xx.sh
+++ target/linux/brcm63xx/base-files/lib/brcm63xx.sh
@@ -96,6 +96,12 @@ brcm63xx_detect() {
 			brcm63xx_has_reset_button="true"
 			ifname="eth0"
 			;;
+		"96369R-1231N")
+			board_model="Comtrend WAP-5813n"
+			status_led="WAP-5813n:green:power"
+			brcm63xx_has_reset_button="true"
+			ifname="eth0"
+			;;
 		"AW4139" |\
 		"AW4339U")
 			board_model="DSL-274XB"
